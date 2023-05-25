cf8363e1b7c8c2a6a0830ee769b8688af096439c
Sleepy <yufei.qiu@se.com>
2023-05-25 09:15:45 +0800

    Add models example

---
+++ 03.Models_TF.py
@@ -0,0 +1,22 @@
+import torch
+from transformers import BertConfig, BertModel
+
+# Building the config
+config = BertConfig()
+
+# Building the model from the config
+model = BertModel(config)
+
+print(config)
+
+model.save_pretrained("directory_on_my_computer")
+
+encoded_sequences = [
+    [101, 7592, 999, 102],
+    [101, 4658, 1012, 102],
+    [101, 3835, 999, 102],
+]
+
+model_inputs = torch.tensor(encoded_sequences)
+
+output = model(model_inputs)
